b46fbe550603e930ca54f291545a9b4bc94ccb3e
Richard Russon (flatcap) <richard.russon@gmail.com>
2013-11-29 02:00:53 +0000

    inline

---
+++ inline.h
@@ -0,0 +1,9 @@
+	Variant (bool           value) { type = Tag::t_unset;  u_bool   = value; }
+	Variant (unsigned char  value) { type = Tag::t_ubyte;  u_ubyte  = value; }
+	Variant (  signed char  value) { type = Tag::t_sbyte;  u_sbyte  = value; }
+	Variant (unsigned short value) { type = Tag::t_ushort; u_ushort = value; }
+	Variant (  signed short value) { type = Tag::t_sshort; u_sshort = value; }
+	Variant (unsigned int   value) { type = Tag::t_uint;   u_uint   = value; }
+	Variant (  signed int   value) { type = Tag::t_sint;   u_sint   = value; }
+	Variant (unsigned long  value) { type = Tag::t_ulong;  u_ulong  = value; }
+	Variant (  signed long  value) { type = Tag::t_slong;  u_slong  = value; }
